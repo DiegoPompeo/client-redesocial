925b86e6027c6bafb24527b82a0c24619f83c1b9
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-10-21 10:54:17 -0300

    restaured config cors

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-    url = 'https://cors-anywhere.herokuapp.com/https://server-redesocial.herokuapp.com/redesocial';
+    url = 'https://server-redesocial.herokuapp.com/redesocial';
 
     constructor(private httpClient: HttpClient, private router: Router) { }
 
