5146ad1272a224415362501110a45b9b86474869
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-10-21 19:39:11 -0300

    update

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ServiceService } from '../service/service.service';
 import { Pessoa } from '../model/pessoa';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-update',
@@ -10,42 +11,75 @@ import { Router } from '@angular/router';
 })
 export class UpdateComponent implements OnInit {
 
+  registerForm: FormGroup;
   pessoa: Pessoa;
+  submitted = false;
+  msgError = false;
+  msgSuccess = false;
+  verificar: boolean;
 
-  constructor(private service: ServiceService, 
-    private router: Router) { }
-
-  ngOnInit() {
-    this.searchProfile();
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router) {
+    this.registerForm = this.formBuilder.group({
+      nome: '',
+      email: '',
+      senha: '',
+      nroCartao: '',
+      nomeNoCartao: '',
+      dataValidade: '',
+      codSeg: '',
+      empresa: ''
+    });
+    this.pessoa = {
+      id: +localStorage.getItem("id"),
+      nome: '',
+      email: '',
+      senha: '',
+      nroCartao: '',
+      nomeNoCartao: '',
+      dataValidade: '',
+      codSeg: '',
+      empresa: '',
+      paga: null
+    }
   }
 
-  downgrade(pessoa: Pessoa){
-    pessoa.paga = false;
-    pessoa.codSeg = '';
-    pessoa.dataValidade = '';
-    pessoa.nomeNoCartao = '';
-    pessoa.nroCartao = '';
-    this.service.atualizarPerfil(pessoa).subscribe(
+  ngOnInit() {    
+  }
+
+  showContaPaga(){
+    this.submitted = true;
+  }
+
+  hideContaPaga(){
+    this.submitted = false;
+  }
+
+  onSubmit() {
+    this.authService.verificaEmail(this.registerForm.get('email').value).subscribe(
       data => {
-        this.pessoa = data;
-        alert("Atualizado com sucesso!");
+        this.verificar = data;
+        if (!this.verificar) {     
+          this.pessoa.nome = this.registerForm.get('nome').value;
+          this.pessoa.email = this.registerForm.get('email').value;
+          this.pessoa.senha = this.registerForm.get('senha').value;
+          this.pessoa.nroCartao = this.registerForm.get('nroCartao').value;
+          this.pessoa.nomeNoCartao = this.registerForm.get('nomeNoCartao').value;
+          this.pessoa.dataValidade = this.registerForm.get('dataValidade').value;
+          this.pessoa.codSeg = this.registerForm.get('codSeg').value;
+          this.pessoa.empresa = this.registerForm.get('empresa').value;
+          this.pessoa.paga = this.submitted;
+    
+          this.authService.register(this.pessoa).subscribe(data => {
+            console.log('register success');            
+          });
+          this.msgSuccess = true;
+        } else {
+          this.msgError = true;
+        }
       }
-    );
-  }
-
-  Atualizar(pessoa: Pessoa){
-    this.service.atualizarPerfil(pessoa).subscribe(
-      data => {
-        this.pessoa = data;
-        this.router.navigate(["/profile"]);
-      }
-    );
-  }
-
-  searchProfile() {
-    this.service.getCientist(localStorage.getItem("email"))
-      .subscribe(data => {
-        this.pessoa = data;
-      });
+    );    
   }
 }
