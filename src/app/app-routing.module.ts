97ca7998b1075f905c9f836ec77e71785164b4a1
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-10-24 14:52:09 -0300

    change something

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RegisterSuccessComponent } from './register-success/register-success.component';
@@ -12,11 +11,12 @@ import { UpdatePremiumComponent } from './update-premium/update-premium.componen
 import { GlossaryComponent } from './glossary/glossary.component';
 import { MembersRecommendedComponent } from './members-recommended/members-recommended.component';
 import { AuthGuard } from './auth.guard';
+import { HomeComponent } from './home/home.component';
 
 // canActivate: [AuthGuard]
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'login', component: LoginComponent},  
+  {path: 'login', component: LoginComponent,},  
   {path: 'register', component: RegisterComponent},
   {path: 'register-success', component: RegisterSuccessComponent},
   {path: 'profile', component: ProfileComponent,canActivate: [AuthGuard]},
