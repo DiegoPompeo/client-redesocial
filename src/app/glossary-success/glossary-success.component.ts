97fa91f982eba4e274d93353fcfdd9c1b9fb521c
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-11-03 20:32:01 -0300

    change

--- src/app/glossary-success/glossary-success.component.ts
+++ src/app/glossary-success/glossary-success.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-glossary-success',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./glossary-success.component.css']
 })
 export class GlossarySuccessComponent implements OnInit {
+
+  @Input()
+  selecionados = [];
 
   constructor() { }
 
