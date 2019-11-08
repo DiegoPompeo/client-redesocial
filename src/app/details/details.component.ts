1af216782b2a1a9311a41491d76bb3d1bca11b1e
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-11-08 00:04:33 -0300

    details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -35,6 +35,7 @@ export class DetailsComponent implements OnInit {
       data => {
         this.pessoa = data;
         this.interesses = data.interesse.split(",");
+        console.log(data.interesse);
       }
     )
   }
