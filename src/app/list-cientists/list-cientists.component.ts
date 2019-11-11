d944760770bde57c21a1017a80e5090d4dacb83e
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-11-10 22:02:27 -0300

    lista

--- src/app/list-cientists/list-cientists.component.ts
+++ src/app/list-cientists/list-cientists.component.ts
@@ -11,17 +11,36 @@ import { Pessoa } from '../model/pessoa';
 export class ListCientistsComponent implements OnInit {
 
   cientist: Pessoa;
-  cientists: Pessoa[];
+  pessoa: Pessoa = new Pessoa();
+  cientists: Pessoa[] = new Array<Pessoa>();
 
   constructor(private service: ServiceService, private router: Router) { }
 
   ngOnInit() {
+    this.getPessoa();
+  }
+
+  listaRecomendado(){
     this.service.listar()
     .subscribe(
       data => {
-        this.cientists = data;
+        let qualidades = [];
+        for (let i = 0; i < data.length; i++) {
+          qualidades = data[i].interesse.split(",");
+          for (let x = 0; x < qualidades.length; x++) {
+            if (this.pessoa.interesse.indexOf(qualidades[x])) {
+              this.cientists.push(data[i]);
+            }
+          }
+        }
       }
     );
+  }
+
+  getPessoa(){
+    this.service.getCientist(localStorage.getItem("email")).subscribe(data => {
+      this.pessoa = data;
+    });
   }
 
   gotoDetails(cientist: Pessoa){
