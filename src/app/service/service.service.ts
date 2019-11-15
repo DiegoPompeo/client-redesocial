ad57a71296071a4f27604332d5b80418747d0c4b
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-11-15 16:38:05 -0300

    commit

--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -11,7 +11,11 @@ export class ServiceService {
   url: string = 'https://server-redesocial.herokuapp.com/redesocial';
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-type': 'application/json'
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+      'Access-Control-Allow-Headers':
+        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
     })
   }
 
