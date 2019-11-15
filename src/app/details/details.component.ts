ddbe02710096e4283033189365d6b10ac78cd6b3
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-11-15 15:19:01 -0300

    data

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -12,6 +12,7 @@ import { AuthService } from '../service/auth.service';
 export class DetailsComponent implements OnInit {
 
   pessoa: Pessoa = new Pessoa();
+  cientista: Pessoa = new Pessoa();
   cientistas: Pessoa[];
   post: Post;
   posts: Post[];
@@ -42,12 +43,12 @@ export class DetailsComponent implements OnInit {
         let str = data.dataNascimento.toString();
         let array = str.split("-");
         let final = array[2] + "/" + array[1] + "/" + array[0];
-        this.pessoa.dataNascimento = final.toString();
+        this.cientista.dataNascimento = final.toString();
 
         let str2 = data.inicioDaAtividade.toString();
         let array2 = str2.split("-");
         let final2 = array2[2] + "/" + array2[1] + "/" + array2[0];
-        this.pessoa.inicioDaAtividade = final2.toString();
+        this.cientista.inicioDaAtividade = final2.toString();
 
         localStorage.setItem("curtidas", data.curtida.toString());
 
