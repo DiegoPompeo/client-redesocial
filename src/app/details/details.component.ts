c10c7ad0c87a63acf7aa1068f8c7d5269d30a419
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-11-17 20:38:51 -0300

    desafazer

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -138,10 +138,12 @@ export class DetailsComponent implements OnInit {
             && data[i].emailRecomendada == localStorage.getItem("det_email")
             && data[i].desfazer == false) {
             this.recomendou = false;
+            break;
           } else if (data[i].emailRecomendou == localStorage.getItem("email")
           && data[i].emailRecomendada == localStorage.getItem("det_email")
           && data[i].desfazer == true) {
             this.recomendou = true;
+            break;
           } else {
             this.recomendou = false;
           }
